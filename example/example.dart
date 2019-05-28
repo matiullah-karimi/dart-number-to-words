7eaad83f95b2507a6e3a12c4abcecfdf0964e85e
Matiullah Karimi <karimi@netlinks.ws>
2019-05-28 10:39:11 +0430

    Add example file

---
+++ example/example.dart
@@ -0,0 +1,9 @@
+import 'package:number_to_words/number_to_words.dart';
+
+void main() {
+    String words = NumberToWords.convert(3, "da");
+    // سه
+
+    String words = NumberToWords.convert(3, "en");
+    // three
+}
--- test/number_to_words_test.dart
+++ test/number_to_words_test.dart
@@ -3,12 +3,12 @@ import 'package:number_to_words/number_to_words.dart';
 
 void main() {
   test('it converts the number to Dari words', () {
-    final number = NumberToWords.convert(3, "da");
-    expect(number, "سه");
+    String words = NumberToWords.convert(3, "da");
+    expect(words.trim(), "سه");
   });
 
   test('it converts the number to English words', () {
-    final number = NumberToWords.convert(3, "en");
-    expect(number, "three");
+    String words = NumberToWords.convert(3, "en");
+    expect(words.trim(), "three");
   });
 }
